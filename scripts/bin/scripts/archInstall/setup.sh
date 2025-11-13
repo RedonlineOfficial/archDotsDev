caa86b4e8fdb95583875e2301d9a92e2deb24836
RedonlineOfficial <dev@redonline.me>
2025-11-13 14:16:04 -0700

    feat(scripts): create setup script for python
    Create a bash script to install python3 and launch the desktopInstall.py
    script.

---
+++ scripts/bin/scripts/archInstall/setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/sudo bash
+# -----------------------------------------------------------
+# Title:    setup.sh
+# Author:   Redonline
+# Updated:  13NOV2025
+# About:    Setup script for the rest of the scripts
+#           as python does not come preinstalled on Arch Linux
+# ------------------------------------------------------------
+
+# Set python version
+pythonVer="3"
+pythonPkg="python${pythonVer}"
+
+# Update
+echo -e "Updating system...\n"
+pacman -Syyu --noconfirm
+echo -e "\nSystem Updated!"
+
+# Install Python
+echo -e "\nInstalling ${pythonPkg}, standby...\n"
+pacman -S --needed --noconfirm ${pythonPkg}
+echo -e "\n${pythonPkg} installed!\n" 
+
+# Start python portion
+${0%/*}/init.py
